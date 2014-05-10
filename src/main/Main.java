f76a3e97b97bc5b59bf3cc70f226f779b4fb40b1
Jack <3montyjack@gmail.com>
2014-05-10 17:01:03 -0500

    Amost working having trouble with array out of bounds exception

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,16 +4,16 @@ import java.io.IOException;
 
 import javax.swing.SwingUtilities;
 
-import envEng.MazeReader;
+import envEng.ItemReader;
 
 public class Main {
 
 	public static void main(String args[]) {
         SwingUtilities.invokeLater(new Runnable() {
                 public void run() {
-                	MazeReader maze = null;
+                	ItemReader maze = null;
 					try {
-						maze = new MazeReader();
+						maze = new ItemReader();
 					} catch (IOException e) {
 						System.out.print("Error in main");
 						e.printStackTrace();
